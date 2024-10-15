107e86b75ac76c0c754ad162c3ba64da5ae8b3d1
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2024-10-14 23:31:51 -0400

    feat(Part-2): client

---
+++ frontend/src/api/index.js
@@ -0,0 +1,34 @@
+var socket = new WebSocket("ws://localhost:8080/ws");
+
+// Connect to the WebSocket endpoint and listen for events
+// Logs connection events, printing out any issues to the
+// browser console
+let connect = () => {
+  console.log("Attempting Connection...");
+
+  socket.onopen = () => {
+    console.log("Successfully Connected.");
+  };
+
+  socket.onmessage = (msg) => {
+    console.log(msg);
+  };
+
+  socket.onclose = (event) => {
+    console.log("Socket Closed Connection: ", event);
+  };
+
+  socket.onerror = (error) => {
+    console.log("Socket Error: ", error);
+  };
+};
+
+// Method to send messages to the backend from the
+// frontend via the WebSocket connection while logging
+// the message
+let sendMsg = (msg) => {
+  console.log("Sending Message: ", msg);
+  socket.send(msg);
+};
+
+export { connect, sendMsg };
